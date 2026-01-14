9aaf757656bbd80855f309b1da98f8d496219225
rony <ronssrony@gmail.com>
2026-01-14 10:14:32 +0600

    add LeaveTypeSeeder and SettingSeeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             DepartmentSeeder::class,
             UserSeeder::class,
-            AttendanceSeeder::class,
+            LeaveTypeSeeder::class,
+            SettingSeeder::class,
         ]);
     }
 }
